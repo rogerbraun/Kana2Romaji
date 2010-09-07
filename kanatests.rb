65bdc7ac0ea27913d57b5ece8903b32a897d9dfd
Roger Braun <davinelulinvega@gmail.com>
2010-09-07 20:42:05 +0200

    More tests

--- kanatests.rb
+++ kanatests.rb
@@ -9,6 +9,10 @@ class Kana2RomajiTest < Test::Unit::TestCase
     assert_equal("かきくけこ".to_romaji, "kakikukeko", "ka-Reihe wird nicht richtig behandelt!")
     assert_equal("さしすせそ".to_romaji, "sashisuseso", "sa-Reihe wird nicht richtig behandelt!")
     assert_equal("まみむめも".to_romaji, "mamimumemo", "ma-Reihe wird nicht richtig behandelt!")
+    assert_equal("たちつてと".to_romaji, "tachitsuteto", "ta-Reihe wird nicht richtig behandelt!")
+    assert_equal("はひふへほ".to_romaji, "hahifuheho", "ha-Reihe wird nicht richtig behandelt!")
+    assert_equal("やゆよわをん".to_romaji, "yayuyowawon", "ya-, wa- und n-Reihe werden nicht richtig behandelt!")
+    assert_equal "なにぬねの".to_romaji, "naninuneno", "na-Reihe wird nicht richtig unterstützt!" 
   end
 
   def test_complicated_kana
